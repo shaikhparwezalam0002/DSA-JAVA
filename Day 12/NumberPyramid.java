2d7c6d8cec25c85ec9c973d8d251e6941e6241e2
Shaikh Parwez Alam <79353520+shaikhparwezalam0002@users.noreply.github.com>
2024-05-29 00:22:24 +0530

    Update NumberPyramid.java

--- Day 12/NumberPyramid.java
+++ Day 12/NumberPyramid.java
@@ -18,3 +18,24 @@ public class NumberPyramid{
         }
     }
 }
+
+/*
+PS D:\Sigma 3.0\assignment\Day 12> java NumberPyramid.java
+5
+    1 
+   2 2 
+  3 3 3 
+ 4 4 4 4 
+5 5 5 5 5 
+PS D:\Sigma 3.0\assignment\Day 12> java NumberPyramid.java
+9
+        1 
+       2 2 
+      3 3 3
+     4 4 4 4
+    5 5 5 5 5
+   6 6 6 6 6 6
+  7 7 7 7 7 7 7
+ 8 8 8 8 8 8 8 8
+9 9 9 9 9 9 9 9 9
+*/
