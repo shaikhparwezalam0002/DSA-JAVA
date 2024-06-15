e83be720fe7832ce9ee5f28e2fd970986fdc89dd
Shaikh Parwez Alam <79353520+shaikhparwezalam0002@users.noreply.github.com>
2024-06-15 14:35:11 +0530

    Create BitWiseOperator.java

---
+++ Day 28 - Bitwise Operator/BitWiseOperator.java
@@ -0,0 +1,16 @@
+public class BitWiseOperator
+{
+	public static void main(String[] args) {
+		System.out.println(4&5); //bitwise AND operator 
+		
+		System.out.println(4|5); //bitwise OR operator
+		
+		System.out.println(4^5);  //bitwise xor operator
+		
+		System.out.println(~4); //bitwise not operator
+		
+		System.out.println(5<<4); // bitwise left shift operator 
+		
+		System.out.println(5>>4); //bitwise right shift operator
+	}
+}
