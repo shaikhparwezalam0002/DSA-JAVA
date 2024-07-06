0dbc0a290201bcaccbb92dde7163f2d6a8a212da
Shaikh Parwez Alam <79353520+shaikhparwezalam0002@users.noreply.github.com>
2024-07-07 05:07:22 +0530

    Create IsArraySorted.java

---
+++ Day 47 - ArrayPractise/IsArraySorted.java
@@ -0,0 +1,15 @@
+// 1752-	Check if the array is sorted -- leetcode
+class Solution {
+    public boolean check(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                c++;
+            }
+        }
+        if(nums[0]<nums[nums.length-1]){
+            c++;
+        }
+        return (c<2) ? true : false;
+    }
+}
